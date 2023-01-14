a600a5b57c5a6251873a2d3ab2acf942e7ecaeef
Eliyas <eliyas.kapasi@gmail.com>
2023-01-14 23:31:57 +0000

    a savepoint for the questions organised in a list

--- assets/java/script.js
+++ assets/java/script.js
@@ -4,7 +4,7 @@ let questionsEl = document.querySelector(".questions")
 let questionTitleEl = document.querySelector("#question-title")
 let starScreenEl = document.querySelector("#start-screen")
 let hiddenEl = document.querySelector(".hide")
-let eachQuestion = document.querySelector("#each-question")
+let questionSet = document.querySelector(".choices")
 
 let questions = [{question: "String values must be enclosed within _______ when being assigned to variables.", answers:["commas", "curly brackets", "quotes", "parenthesis"], correctAnswer: 2}
 
@@ -46,24 +46,43 @@ function startTimer() {
 //This function renders the questions 
 function renderQuestions() {
     //This hides or removes anything that should not be shown once the start button is pressed
-    starScreenEl.setAttribute("style", "display: none")
-    hiddenEl.setAttribute("data-state", "hidden")
+    starScreenEl.classList.add("hide")
+//    hiddenEl.setAttribute("data-state", "hidden")
 
     //This renders the questions for the quiz
     questionsEl.setAttribute("data-state", "visible")
     questionTitleEl.textContent = questions[currentQuestion].question
     console.log(questions[currentQuestion].answers.length)
+
+    let ol = document.createElement("ol");
+    questionSet.appendChild(ol);
+
+
     for (let i = 0; i < questions[currentQuestion].answers.length; i++) {
-        let li = document.createElement("li"); 
-    //    li.textContent = questions[currentQuestion].answers[i]
+
+        let li = document.createElement("li");
 
         let button = document.createElement("button");
-        button.textContent = questions[currentQuestion].answers[i]
-
         li.setAttribute("data-index", i);
 
+
+//       button.appendChild(ul)
+
+        ol.appendChild(li);
         li.appendChild(button);
-        eachQuestion.appendChild(li)
+        button.textContent = questions[currentQuestion].answers[i]
+
+
+//        let li = document.createElement("li"); 
+    //    li.textContent = questions[currentQuestion].answers[i]
+
+//        let button = document.createElement("button");
+//       button.textContent = questions[currentQuestion].answers[i]
+
+//        li.setAttribute("data-index", i);
+
+//        li.appendChild(button);
+//        eachQuestion.appendChild(li)
 
     }
 
