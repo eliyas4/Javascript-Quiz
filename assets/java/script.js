fb1946fc1986ea7c517bc821ca129f0da0bf8ac6
Eliyas <eliyas.kapasi@gmail.com>
2023-01-17 01:27:54 +0000

    Removed all the commented code to make the javaFile clearer

--- assets/java/script.js
+++ assets/java/script.js
@@ -18,47 +18,16 @@ let questions = [
 ]
 console.log(questions[0].question)
 console.log(questions[0].answers)
-let currentQuestion = 4;
+let currentQuestion = 3;
 let timerCount = 90;
 
 //The game starts when the start button is pressed
 startButtonEl.addEventListener("click",startGame);
-//gameLogic();
-
-
-
-
 
 //This function is called when the start button is pressed
 function startGame() {
-    console.log("cur", currentQuestion)
     startTimer()
     renderQuestions()
-
-    
-
-//    return
-//   renderQuestions()
-        
-
-
-    console.log("cur", currentQuestion)
-//    renderQuestions()
-
-
-
-
-
-
-
-//    renderQuestions()
-
-
-        
-
-    
-
-
     startButtonEl.disabled = true;
 }
 //This function is called if the timer runs out
@@ -71,15 +40,6 @@ function winGame() {
 }
 
 
-
-
-
-
-
-
-
-
-
 // The startTimer function starts and stops the timer and triggers winGame() and loseGame()
 function startTimer() {
     timer = setInterval(function(){
@@ -89,11 +49,11 @@ function startTimer() {
     
 }
 
+
 //This function renders the questions 
 function renderQuestions() {
     //This hides or removes anything that should not be shown once the start button is pressed
     starScreenEl.classList.add("hide")
-//    hiddenEl.setAttribute("data-state", "hidden")
 
     //This renders the questions for the quiz
     questionsEl.setAttribute("data-state", "visible")
@@ -107,83 +67,34 @@ function renderQuestions() {
 
     for (let i = 0; i < questions[currentQuestion].answers.length; i++) {
 
-//       let li = document.createElement("li");
-
         let button = document.createElement("button");
         button.setAttribute("style", "width: 30%");
- //       button.setAttribute("style", "align-items: left");
         button.setAttribute("data-index", i);
 
-
-//       button.appendChild(ul)
-
         ol.appendChild(button);
-//        li.appendChild(button);
         button.textContent =((i + 1) + ". " + questions[currentQuestion].answers[i])
     }
-//    gameLogic();
-//    currentQuestion += 1
-//    console.log("cur", currentQuestion)
 
     questionsEl.addEventListener("click", function(event) {
         let element = event.target;
         if (element.matches("button") === true) {
-
-            let index = element.getAttribute("data-index");
-    //           console.log(index)
             feedbackEl.classList.remove("hide")
+            let index = element.getAttribute("data-index");
+
             if (index == questions[currentQuestion].correctAnswer) {
-    //                console.log("DDD")
-
                 feedbackEl.textContent = "Correct Answer"
-
-                
-//                currentQuestion += 1
-                resetState()
-
-                console.log("ghbj" , currentQuestion)
-//                renderQuestions()
-
-
-
-
+                nextQuestion()
             }
-            else {
-                
+            else {          
                 feedbackEl.textContent = "Incorrect Answer"
                 timerCount -= 20; 
-
-
-
-                
-//                currentQuestion += 1
-                resetState()
-
-
-                console.log("ghbj" , currentQuestion)
-//                renderQuestions()
-
-
-                
-
-
-
-
+                nextQuestion()
             }
-    //             
-    //   
-            
         }
-
-//      renderQuestions()  
     });
-    //    renderQuestions()
-    //    gameLogic()
-
-
 }
 
-function resetState() {
+function resetQuestions() {
     while (questionSet.firstChild) {
         questionSet.removeChild
         (questionSet.firstChild)
@@ -191,67 +102,11 @@ function resetState() {
     while (questionTitleEl.firstChild) {
         questionTitleEl.removeChild
         (questionTitleEl.firstChild)
-    }
+    }    
+}
 
-//    feedbackEl.classList.add("hide")
-        
+function nextQuestion() {
+    resetQuestions()
 //    renderQuestions()   
-    
-
-    
 }
-
-
-
-/*
-
-function gameLogic() {
-    questionsEl.addEventListener("click", function(event) {
-        let element = event.target;
-        if (element.matches("button") === true) {
-
-            let index = element.getAttribute("data-index");
-//           console.log(index)
-            feedbackEl.classList.remove("hide")
-            if (index == questions[currentQuestion].correctAnswer) {
-//                console.log("DDD")
-
-                feedbackEl.textContent = "Correct Answer"
-//                currentQuestion += 1
-
-
-            }
-            else {
-                
-                feedbackEl.textContent = "Incorrect Answer"
-                 timerCount -= 20; 
-//                 currentQuestion += 1
-                 renderQuestions()
-
-            }
-//             
-//            renderQuestions()   
-        }
-
-    });
-//    renderQuestions()
-//    gameLogic()
-
-
-
-}
-console.log("cur", currentQuestion)
-
-
-
-
-
-*/
-
-
-
-
-
-//These are all the event listeners 
-
 
