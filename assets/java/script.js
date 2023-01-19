feb25ddabcfd01f7d95806b6040cbf55a997e86d
Eliyas <eliyas.kapasi@gmail.com>
2023-01-19 20:26:33 +0000

    added a loose game page

--- assets/java/script.js
+++ assets/java/script.js
@@ -6,6 +6,7 @@ let starScreenEl = document.querySelector("#start-screen")
 let hiddenEl = document.querySelector(".hide")
 let questionSet = document.querySelector(".choices")
 let feedbackEl = document.querySelector("#feedback")
+let looseScreenEl = document.querySelector("#loose-game")
 
 let questions = [
     {question: "What are comets mostly made of?", answers:["Snow, ice, and dust", "Various metals", "Poisonous liquid", "Light"], correctAnswer: 0},
@@ -13,7 +14,7 @@ let questions = [
     {question: "How much money does a NASA space suit cost?", answers:["$10,000", "$5,000", "$1 Million", "12 Million"], correctAnswer: 3},
     {question: "The surface of planet 55 cancri e primarily consists of what substance?", answers:["Rubber", "Water", "Diamond", "Lava"], correctAnswer: 2},
     {question: "Is Pluto a planet?", answers:["Yes", "No... but it should be!"], correctAnswer: 1},
-
+    {question: "How long do footprints on the moon last?", answers:["100 years", "10,000 years", "1 million years", "100 million years"], correctAnswer: 3},
 ]
 
 let currentQuestion = 0;
@@ -24,13 +25,31 @@ startButtonEl.addEventListener("click",startGame);
 
 //This function is called when the start button is pressed
 function startGame() {
-    startTimer()
-    removeStartScreen()
-    generateQuiz()
+    startTimer();
+    removeStartScreen();
+    generateQuiz();
+    looseGame();
+    
     startButtonEl.disabled = true;
 }
+
 //This function is called if the timer runs out
 function looseGame() {
+    //console.log("firing")
+    //console.log(timerCount)
+    if (timerCount < 0) {
+        resetQuestions()
+        console.log("firing")
+        looseScreenEl.classList.remove("hide")
+        let restartButtonEl = document.querySelector("#restart")
+        restartButtonEl.addEventListener("click", () => {
+
+            
+
+        })
+
+
+    }
     
 }
 //This function is called if all the questions are answered and the timer does not run out
@@ -44,6 +63,8 @@ function startTimer() {
     timer = setInterval(function(){
         timerCount--;
         timerCountEl.textContent = timerCount;
+        looseGame()
+
     }, 1000);
     
 }
@@ -77,31 +98,29 @@ function generateQuiz() {
         ol.appendChild(button);
         button.textContent =((i + 1) + ". " + questions[currentQuestion].answers[i])
     }
-
-    questionsEl.addEventListener("click", function(event) {
-        let element = event.target;
-        if (element.matches("button") === true) {
-            feedbackEl.classList.remove("hide")
-            let index = element.getAttribute("data-index");
-
-            if (index == questions[currentQuestion].correctAnswer) {
-                feedbackEl.textContent = "Correct Answer"
-                setTimeout(resetQuestions, 1500);
-            }
-            else {          
-                feedbackEl.textContent = "Incorrect Answer"
-                timerCount -= 20; 
-                setTimeout(resetQuestions, 1500);
-            }    
-        }
-    });
 }
 
-//setTimeout(resetQuestions, 1500);
-//setTimeout(generateQuiz, 3000)
-//currentQuestion++
-//console.log("current question is", currentQuestion)
 
+    
+questionsEl.addEventListener("click", function(event) {
+    element = event.target;
+    if (element.matches("button") === true) {
+        feedbackEl.classList.remove("hide")
+        let index = element.getAttribute("data-index");
+
+        if (index == questions[currentQuestion].correctAnswer) {
+            feedbackEl.textContent = "Correct Answer"
+            setTimeout(resetQuestions, 1500);
+            setTimeout(nextQuestion, 1500);
+        }
+        else {          
+            feedbackEl.textContent = "Incorrect Answer"
+            timerCount -= 20; 
+            setTimeout(resetQuestions, 1500);
+            setTimeout(nextQuestion, 1500);
+        }    
+    }
+});
 
 function resetQuestions() {
     while (questionSet.firstChild) {
@@ -115,10 +134,11 @@ function resetQuestions() {
     feedbackEl.classList.add("hide")
 }
 
-
-
 function nextQuestion() {
-
+    if (element.matches("button") === true) {
+        currentQuestion++
+        generateQuiz() 
+    }
 }
 
 
