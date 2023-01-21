f921368b174e994e1b4f5132386757e42349c96f
Eliyas <eliyas.kapasi@gmail.com>
2023-01-21 02:23:01 +0000

    saved the highscores to the local storage

--- assets/java/highscore.js
+++ assets/java/highscore.js
@@ -1,13 +1,19 @@
-
-
-function displayHighscore() {
+function getHighScore() {
     resetQuestions()
+    winScreenEl.classList.remove("hide")
     let winingUser = prompt("Congratulations, you won!!!", "Please enter your name to save your score.");
     highScores.push({
         name: winingUser,
         score: timerCount,
     }) 
-    let highScoreList = JSON.setItem("highScores", JSON.stringify(highScores))
-    location.replace("https://eliyas4.github.io/Javascript-Quiz/highscores.html")
-    console.log("firing")
+}
+
+function displayHighscore() {
+    localStorage.setItem("highScores", JSON.stringify(highScores))
+    let storedHighscores = JSON.parse(localStorage.getItem("highScores"))
+    for (let i = 0; i < storedHighscores.length; i++) {
+        let li = document.createElement("li");  
+        li.innerText = `${highScores[i].name} - ${highScores[i].score}`;
+        scoreSheetEl.appendChild(li);   
+    }  
 }
--- assets/java/script.js
+++ assets/java/script.js
@@ -7,38 +7,36 @@ let hiddenEl = document.querySelector(".hide")
 let questionSet = document.querySelector(".choices")
 let feedbackEl = document.querySelector("#feedback")
 let looseScreenEl = document.querySelector("#loose-game")
+let scoreSheetEl = document.querySelector("#highscores") 
+let winScreenEl = document.querySelector("#win-screen")
+let clearButtonEl = document.querySelector("#clear")
 
 let questions = [
     {question: "What are comets mostly made of?", answers:["Snow, ice, and dust", "Various metals", "Poisonous liquid", "Light"], correctAnswer: 0},
-//    {question: "Which planet has rings around it?", answers:["Earth", "Jupiter", "Saturn", "Uranus"], correctAnswer: 2},
-//    {question: "How much money does a NASA space suit cost?", answers:["$10,000", "$5,000", "$1 Million", "12 Million"], correctAnswer: 3},
-//    {question: "The surface of planet 55 cancri e primarily consists of what substance?", answers:["Rubber", "Water", "Diamond", "Lava"], correctAnswer: 2},
-//    {question: "Is Pluto a planet?", answers:["Yes", "No... but it should be!"], correctAnswer: 1},
-//    {question: "How long do footprints on the moon last?", answers:["100 years", "10,000 years", "1 million years", "100 million years"], correctAnswer: 3},
+    {question: "Which planet has rings around it?", answers:["Earth", "Jupiter", "Saturn", "Uranus"], correctAnswer: 2},
+    {question: "How much money does a NASA space suit cost?", answers:["$10,000", "$5,000", "$1 Million", "12 Million"], correctAnswer: 3},
+    {question: "The surface of planet 55 cancri e primarily consists of what substance?", answers:["Rubber", "Water", "Diamond", "Lava"], correctAnswer: 2},
+    {question: "Is Pluto a planet?", answers:["Yes", "No... but it should be!"], correctAnswer: 1},
+    {question: "How long do footprints on the moon last?", answers:["100 years", "10,000 years", "1 million years", "100 million years"], correctAnswer: 3},
 ]
 
 let currentQuestion = 0;
 let timerCount = 90;
 let highScores = []
 
-//The game starts when the start button is pressed
-startButtonEl.addEventListener("click",startGame);
+//These are all the event listeners
+startButtonEl.addEventListener("click", startGame);
 
 //This function is called when the start button is pressed
 function startGame() {
     startTimer();
     removeStartScreen();
     generateQuiz();
-//    looseGame();
-    winGame();
-    
     startButtonEl.disabled = true;
 }
 
 //This function is called if the timer runs out
 function looseGame() {
-    //console.log("firing")
-    //console.log(timerCount)
     if (timerCount < 0) {
         resetQuestions()
         console.log("firing")
@@ -46,32 +44,32 @@ function looseGame() {
         let restartButtonEl = document.querySelector("#restart")
         restartButtonEl.addEventListener("click", () => {
         })
+        clearInterval(timer)
     }
-    
 }
+
 //This function is called if all the questions are answered and the timer does not run out
 function winGame() {
     if (timerCount >= 0 && currentQuestion >= questions.length) {
+        clearInterval(timer)
+        getHighScore();
         displayHighscore();
-        console.log("firing")
-        
     }
-
 }
-
 
 // The startTimer function starts and stops the timer and triggers winGame() and loseGame()
 function startTimer() {
     timer = setInterval(function(){
         timerCount--;
         timerCountEl.textContent = timerCount;
-        looseGame();
+        if (timerCount >= 0 && currentQuestion >= questions.length) {
         winGame();
-
+        }
+        if(timerCount <0) {
+            looseGame()
+        }
     }, 1000);
-    
 }
-
 
 //This function renders the questions 
 function removeStartScreen() {
@@ -79,16 +77,9 @@ function removeStartScreen() {
     starScreenEl.classList.add("hide")
     //This renders the questions for the quiz
     questionsEl.setAttribute("data-state", "visible")
-
-
 }
 
-
-
 function generateQuiz() {
-//    console.log("current question is", currentQuestion)
-
-
     ol = document.createElement("ol");
     questionSet.appendChild(ol);
     questionTitleEl.textContent = questions[currentQuestion].question
@@ -102,9 +93,7 @@ function generateQuiz() {
         button.textContent =((i + 1) + ". " + questions[currentQuestion].answers[i])
     }
 }
-
-
-    
+  
 questionsEl.addEventListener("click", function(event) {
     element = event.target;
     if (element.matches("button") === true) {
@@ -140,10 +129,9 @@ function resetQuestions() {
 function nextQuestion() {
     if (element.matches("button") === true) {
         currentQuestion++
-        generateQuiz() 
-    }
+        
+        if (!(timerCount >= 0 && currentQuestion >= questions.length || timerCount <0)) {
+            generateQuiz()
+        }
+    }  
 }
-
-
-
-
